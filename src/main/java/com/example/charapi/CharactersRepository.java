33442927c94c4321244c6424c2a4284426070b03
Melinda Garren <mgarren84@gmail.com>
2026-07-05 10:13:46 -0400

    correcting filename error that caused linking error.

--- src/main/java/com/example/charapi/CharactersRepository.java
+++ src/main/java/com/example/charapi/CharactersRepository.java
@@ -3,7 +3,9 @@ package com.example.charapi;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CharactersRepository extends JpaRepository<Characters, Long> {
     List<Characters> findByNameContainingIgnoreCase(String name);
     
